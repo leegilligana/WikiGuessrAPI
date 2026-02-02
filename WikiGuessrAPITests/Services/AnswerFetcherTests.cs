36f84c2f772c4c29e0ed866108879f7866b8d603
leegilligana <aidanleegilligan@gmail.com>
2026-02-02 10:45:18 +0100

    added moq and xunit, added an empty test file

---
+++ WikiGuessrAPITests/Services/AnswerFetcherTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace WikiGuessrAPITests.Services;
+
+public class AnswerFetcherTests
+{
+    [Fact]
+    public void ShouldReturnValidId()
+    {
+        return;
+    }
+
+    [Fact]
+    public void ShouldThrowException()
+    {
+        return;
+    }
+}
