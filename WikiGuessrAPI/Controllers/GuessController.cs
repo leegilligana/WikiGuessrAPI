d8dfd356aa35b55ab6180af878fba23e78e09331
leegilligana <aidanleegilligan@gmail.com>
2026-02-02 10:22:03 +0100

    Changed launch settings to add open api launch on startup, added dummy guess controller for testing

---
+++ WikiGuessrAPI/Controllers/GuessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WikiGuessrAPI.Models;
+
+namespace WikiGuessrAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GuessController : ControllerBase
+{
+    [HttpPost(Name = "PostGuess")]
+    public ActionResult PostGuess([FromBody]GuessSubmission guess) => Ok();
+
+    [HttpGet(Name = "GetGuess")]
+    public ActionResult Get() => Ok();
+}
---
+++ WikiGuessrAPI/Models/GuessSubmission.cs
@@ -0,0 +1,12 @@
+﻿namespace WikiGuessrAPI.Models;
+
+internal class GuessSubmission
+{
+    public int PlayerId { get; init; }
+
+    public double Latitude { get; init; }
+
+    public double Longitude { get; init; }
+
+    public TimeSpan TimeTaken { get; init; }
+}
